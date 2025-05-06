4edda8d206b15e2e4ba125b7e3698dbc475ff4d6
adn67 <alejandro_di_nardo@hotmail.com>
2025-05-06 17:14:39 -0300

    modificación en archivos [slug] y astro.config.mjs para renderizar HTML en .md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
 import { defineConfig } from "astro/config";
+import remarkGfm from "remark-gfm";
 
 export default defineConfig({
+  //genera un sitio estático
   output: "static",
-  // si eliges servir desde docs/:
-  build: {
-    dist: "docs",
+
+  // Configuración directa de Vite para que el build escriba en docs/
+  // (¡No lo hace!)
+  vite: {
+    build: {
+      outDir: "docs",
+    },
+    // opcional para publicar en GitHub Pages:
+    //site: "https://adn.github.io/astro-taxonomia-md",
+    //base: "/astro-taxonomia-md/",
   },
-  // opcional:
-  site: "https://adn.github.io/astro-taxonomia-md",
+
+  markdown: {
+    remarkPlugins: [remarkGfm],
+    syntaxHighlight: 'prism',   // para tener resaltado de código
+  },
 });
