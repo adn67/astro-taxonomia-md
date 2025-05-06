5a03c94327ba34f2c9f5dff31ad63b383c241b4f
adn67 <alejandro_di_nardo@hotmail.com>
2025-05-06 17:54:11 -0300

    modifico astro.config.mjs para deploy en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,24 @@
 import { defineConfig } from "astro/config";
 import remarkGfm from "remark-gfm";
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig({
   //genera un sitio estático
   output: "static",
 
-  // Configuración directa de Vite para que el build escriba en docs/
-  // (¡No lo hace!)
-  vite: {
-    build: {
-      outDir: "docs",
-    },
-    // opcional para publicar en GitHub Pages:
-    //site: "https://adn.github.io/astro-taxonomia-md",
-    //base: "/astro-taxonomia-md/",
+// Para GH Pages, base = nombre del repo. Para Netlify (u otro), base = '/'
+base: isGitHubPages ? '/astro-taxonomia-md/' : '/',
+vite: {
+  build: {
+    outDir: 'docs',   // si usas docs/ para GH Pages
   },
+},
+// Site no es estrictamente necesario, pero puedes definirlo:
+site: isGitHubPages
+  ? 'https://adn67.github.io/astro-taxonomia-md/'
+  : undefined,
+
 
   markdown: {
     remarkPlugins: [remarkGfm],
